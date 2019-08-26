10951c984f46ff34f4ebaf448302a7d89de752c4
Flafi87 <laaszlo.gaal@gmail.com>
2019-08-26 14:05:22 +0200

    npm update and design

--- src/components/App.js
+++ src/components/App.js
@@ -1,61 +1,70 @@
-import React from "react";
-import Axios from "axios";
-import { Button } from 'reactstrap';
-import Mapa from "./Map";
+import React from 'react';
+import Axios from 'axios';
+import { Button, Spinner } from 'reactstrap';
+import Mapa from './Map';
 
 class App extends React.Component {
-  state = {
-    weather: [],
-    forecast: [],
-    isLoaded: false
-  };
+  buttons = (
+    <div className="d-flex justify-content-between">
+      <Button className="my-3" color="success" href="https://flafi.me/index.html#jscript">
+        Back to the website
+      </Button>
+      <Button
+        className="my-3"
+        color="primary"
+        href="https://github.com/Flafi87/weathermap"
+        target="_blank"
+      >
+        Repo
+      </Button>
+    </div>
+  );
+
+  spinner = (
+    <div className="d-flex justify-content-center align-middle">
+      <Spinner color="primary" />
+    </div>
+  );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: [],
+      forecast: [],
+      isLoaded: false,
+    };
+  }
 
   componentDidMount() {
-    Axios("https://flafi.me:2053/api/weather").then(
+    Axios('https://flafi.me:2053/api/weather').then(
       weather => {
         this.setState({ weather: weather.data });
       },
       error => {
         console.log(error);
-      }
+      },
     );
-    Axios("https://flafi.me:2053/api/forecast").then(
+    Axios('https://flafi.me:2053/api/forecast').then(
       forecast => {
         this.setState({ forecast: forecast.data });
         this.setState({ isLoaded: true });
       },
       error => {
         console.log(error);
-      }
+      },
     );
   }
 
   render() {
-      const {weather, forecast, isLoaded} = this.state
+    const { weather, forecast, isLoaded } = this.state;
     return (
       <div className="container">
-        <div className="d-flex justify-content-between">
-          <Button
-            className="my-3"
-            color="success"
-            href="https://flafi.me/#jscript"
-          >
-      Back to the website
-          </Button>
-          <Button
-            className="my-3"
-            color="primary"
-            href="https://github.com/Flafi87/weathermap"
-          >
-      Repo
-          </Button>
-        </div>
-        
-        <Mapa
-          weather={weather}
-          forecast={forecast}
-          isLoaded={isLoaded}
-        />
+        {this.buttons}
+        {isLoaded ? (
+          <Mapa weather={weather} forecast={forecast} isLoaded={isLoaded} />
+        ) : (
+          this.spinner
+        )}
       </div>
     );
   }
--- src/components/TempChart.js
+++ src/components/TempChart.js
@@ -1,21 +1,16 @@
-import React, { Component } from "react";
-import Chart from "react-apexcharts";
+import React, { Component } from 'react';
+import Chart from 'react-apexcharts';
 import PropTypes from 'prop-types';
 
 class TempChart extends Component {
-
-  static propTypes = {
-    forecast:PropTypes.arrayOf(PropTypes.object).isRequired
-  } 
-
   constructor(props) {
     super(props);
     this.state = {
       options: {
         chart: {
-          id: "line-chart",
-          width: "100%",
-          height: "100%",
+          id: 'line-chart',
+          width: '100%',
+          height: '100%',
           toolbar: {
             show: false,
             tools: {
@@ -27,42 +22,42 @@ class TempChart extends Component {
               pan: false,
               // eslint-disable-next-line no-bitwise
               reset: false | '<img src="/static/icons/reset.png" width="20">',
-              customIcons: []
+              customIcons: [],
             },
-            autoSelected: "zoom"
-          }
+            autoSelected: 'zoom',
+          },
         },
         xaxis: {
-          type: "datetime",
+          type: 'datetime',
           tooltip: {
-            enabled: false
-          }
+            enabled: false,
+          },
         },
         yaxis: [
           {
             title: {
-              text: "Temperature"
+              text: 'Temperature',
             },
             labels: {
               formatter(value) {
-                return `${value.toFixed(1)  } C°`;
-              }
-            }
+                return `${value.toFixed(1)} C°`;
+              },
+            },
           },
           {
             opposite: true,
             title: {
-              text: "Rain"
+              text: 'Rain',
             },
             labels: {
               formatter(value) {
-                return `${value.toFixed(1)  } mm`;
-              }
-            }
-          }
+                return `${value.toFixed(1)} mm`;
+              },
+            },
+          },
         ],
         legend: {
-          offsetY: -10
+          offsetY: -10,
         },
         responsive: [
           {
@@ -70,58 +65,57 @@ class TempChart extends Component {
             options: {
               plotOptions: {
                 bar: {
-                  horizontal: false
-                }
+                  horizontal: false,
+                },
               },
               legend: {
-                position: "bottom"
-              }
-            }
-          }
+                position: 'bottom',
+              },
+            },
+          },
         ],
         tooltip: {
           enabled: false,
           shared: true,
-          theme: "light",
+          theme: 'light',
           onDatasetHover: {
-            highlightDataSeries: false
+            highlightDataSeries: false,
           },
           x: {
             show: true,
-            format: "dd MMM HH:mm",
-            formatter: undefined
+            format: 'dd MMM HH:mm',
+            formatter: undefined,
           },
           marker: {
-            show: true
-          }
+            show: true,
+          },
         },
 
         stroke: {
           show: true,
-          curve: "smooth",
-          lineCap: "butt",
+          curve: 'smooth',
+          lineCap: 'butt',
           colors: undefined,
           width: 2,
-          dashArray: 0
-        }
-      }
+          dashArray: 0,
+        },
+      },
     };
   }
 
   render() {
-    const {forecast} = this.props
-    const {options} = this.state
+    const { forecast } = this.props;
+    const { options } = this.state;
     if (!forecast) {
       return <div>Loading...</div>;
     }
     const forecastTemp = [];
     const forecastRain = [];
     function checker(x) {
-      if (x.rain && x.rain["3h"] !== undefined) {
-        return x.rain["3h"];
-      } 
-        return 0;
-      
+      if (x.rain && x.rain['3h'] !== undefined) {
+        return x.rain['3h'];
+      }
+      return 0;
     }
     forecast.forEach(function(object) {
       forecastTemp.push([object.dt * 1000, object.main.temp]);
@@ -132,13 +126,17 @@ class TempChart extends Component {
         <Chart
           options={options}
           series={[
-            { name: "Temperature", type: "line", data: forecastTemp },
-            { name: "Rain", type: "bar", data: forecastRain }
+            { name: 'Temperature', type: 'line', data: forecastTemp },
+            { name: 'Rain', type: 'bar', data: forecastRain },
           ]}
         />
       </div>
     );
   }
 }
+
+TempChart.propTypes = {
+  forecast: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default TempChart;
